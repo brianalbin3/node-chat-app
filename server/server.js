efc6336be97e2cabfafc6998a07c629547514467
brianalbin3 <brianalbin3@gmail.com>
2017-05-17 13:47:18 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -11,11 +11,24 @@ var io = socketIO(server);
 
 //socket.emit - single
 //io.emit - everyone
+//socket.broadcast.emit - everyone but myself
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 	console.log('New user connected');
+
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app!',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined!',
+		createdAt: new Date().getTime()
+	});
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
